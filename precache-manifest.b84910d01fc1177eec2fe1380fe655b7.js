b2a796f18c822be1f0d4493c6d8dceebca729518
Sam <git@cksum.co.uk>
2020-10-17 21:19:27 +0100

    Updates

---
+++ precache-manifest.b84910d01fc1177eec2fe1380fe655b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6415f531818bb68f7dd8d953eea0148d",
+    "url": "/react-reddit/index.html"
+  },
+  {
+    "revision": "6613071ce6cf6bcf23d8",
+    "url": "/react-reddit/static/css/main.481495db.chunk.css"
+  },
+  {
+    "revision": "7e99c16f56e814fc0b10",
+    "url": "/react-reddit/static/js/2.a956f7df.chunk.js"
+  },
+  {
+    "revision": "f4397f88bfd4c390e3817518294be012",
+    "url": "/react-reddit/static/js/2.a956f7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6613071ce6cf6bcf23d8",
+    "url": "/react-reddit/static/js/main.a927fa2b.chunk.js"
+  },
+  {
+    "revision": "587d5a141e46f88c60fe",
+    "url": "/react-reddit/static/js/runtime-main.234bd2fc.js"
+  },
+  {
+    "revision": "05cec0841f726cac955e24e64a06ea48",
+    "url": "/react-reddit/static/media/logo.05cec084.gif"
+  }
+]);
